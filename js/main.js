eb554e4c7c18ad9bc346e099249721c11d9efeff
sukumar k <sukumarkumaravel@gmail.com>
2025-11-26 16:22:08 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -50,3 +50,66 @@
 
 })(jQuery);
 
+// Form Details
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let subject = document.getElementById("subject").value.trim();
+  let message = document.getElementById("message").value.trim();
+  let error = "";
+
+  if (name.length < 3) {
+    error = "Name must be at least 3 characters.";
+  } else if (!email.includes("@") || !email.includes(".")) {
+    error = "Enter a valid email.";
+  } else if (message.length < 10) {
+    error = "Message should be at least 10 characters.";
+  }
+
+  if (error) {
+    document.getElementById("error").innerText = error;
+  } else {
+    alert("Message sent successfully!");
+    document.getElementById("error").innerText = "";
+    document.getElementById("contactForm").reset();
+  }
+});
+
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "61f6bbfc-f6d9-4520-afa5-9b96f89f0767");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
